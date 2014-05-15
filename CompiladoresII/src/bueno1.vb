d08ac3e907362c0f4824d99ed4012e7f11bad30e
Ricardo Erazo <erazo.14@hotmail.com>
2014-05-15 08:29:16 -0600

    poco de comprobacion de tipos

--- CompiladoresII/src/bueno1.vb
+++ CompiladoresII/src/bueno1.vb
@@ -2,6 +2,11 @@ Dim d,f,x As Integer
 Dim y As Integer
 Dim z As Integer
 
+type hola
+a as char
+a as integer
+end type
+
 Sub Sumar(byval x As Integer , Byval y As Integer)  'comentario
 Dim t As char
 t.f = (x.b + y) +  9 + ((9 + 3) + (7+7))
@@ -9,16 +14,18 @@ input (t)
 
 end sub
 
-dim a char
-
 Sub Main()
 Print "hola" _
 & " juan"
 x = "agregando" & "hola"
 y= 20 
 
-Call Sumar(10,20)
+dim gy as hola
 
+
+Call Sumar(10,20)
+if 4>3 then 
+end if 
 
 End Sub
 
